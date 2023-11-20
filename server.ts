bcbc3d8073686bd24db0a538a7f71dee22836889
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-11-21 06:03:00 +0800

    feat: show data range of job samples

--- server.ts
+++ server.ts
@@ -16,8 +16,8 @@ app.listen(port, () => {
 
 let select_range = db.prepare(/* sql */ `
 select
-  min(post_time) as min
-, max(post_time) as max
+  min(post_time) as since
+, max(post_time) as until
 from job
 `)
 
@@ -35,7 +35,10 @@ order by
 `)
 
 type Data = {
-  range: { min: string; max: string }
+  range: {
+    since: string
+    until: string
+  }
   words: {
     word: string
     job_count: number
@@ -51,8 +54,20 @@ function getData(): Data {
 
 function readTemplate() {
   let html = readFileSync('public/index.html').toString()
-  let [before, after] = html.split('{tbody}')
+
+  let parts = html.split('{since}')
+  let p1 = parts[0]
+
+  parts = parts[1].split('{until}')
+  let p2 = parts[0]
+
+  parts = parts[1].split('{tbody}')
+  let p3 = parts[0]
+
+  let p4 = parts[1]
+
   function render(data: Data): string {
+    let { range } = data
     let tbody = ''
     for (let word of data.words) {
       tbody += /* html */ `
@@ -63,7 +78,7 @@ function readTemplate() {
 </tr>
 `
     }
-    return `${before}${tbody}${after}`
+    return `${p1}${range.since}${p2}${range.until}${p3}${tbody}${p4}`
   }
   return { render }
 }
