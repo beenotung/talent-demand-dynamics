c1de8d22e5ce2b4ef63fc605c17e62265be310d8
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-11-27 17:08:08 +0800

    patch: fix edge case weekly_job_stat.sql

--- analysis/weekly_job_stat.sql
+++ analysis/weekly_job_stat.sql
@@ -1,5 +1,11 @@
 select
   count(*) as count
-, strftime('%Y', post_time) || '-' || strftime('%m', post_time) || '-' || strftime('%W', post_time) as year_month_week
+, strftime('%Y', post_time) as year
+, strftime('%W', post_time) as week
+, min(date(post_time)) as 'since'
+, max(date(post_time)) as 'until'
 from job
-group by year_month_week
+group by strftime('%Y-%W', post_time)
+having min(strftime('%w', post_time)) = '0'
+   and max(strftime('%w', post_time)) = '6'
+order by since asc
