3da5b058532596969dd339f73bf31885fae4b92c
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-12-03 08:22:26 +0800

    chore: install dep before running knex migration

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,11 +15,12 @@ rsync -SavLPz \
 
 ssh "$USER@$HOST" "
 source ~/.nvm/nvm.sh && \
+cd $PROJECT_DIR && \
+pnpm i --prod --no-optional && \
 cd $PROJECT_DIR/dist && \
 rm -f db.sqlite3 && \
 ln -s ../db.sqlite3 && \
 npx knex migrate:latest && \
 cd .. && \
-pnpm i --prod --no-optional && \
 pm2 reload talent-demand-dynamics
 "
